6d773ea7f02cb5759b8c6dcb8e14c08c1b66555d
Clement <clem639@hotmail.com>
2018-06-12 18:33:04 -0500

    db class added

--- class.listsPage.php
+++ class.listsPage.php
@@ -7,23 +7,73 @@
 		public $checked=null;
 
 		private $db=null;
+		private $table=null;
 
-		public function __construct(db $db=null){
+		public function __construct(
+								db $db=null,
+								list $list=null,
+								string $name=null,
+								int $id=null,
+								boolean $checked=null){
 			if(isset($db)){
 				$this->db = $db;
 			}else{
 				$this->db = new db();
 			}
+			$this->table = "list_items";
+			$this->list = $list;
+			$this->name = $name;
+			$this->id = $id;
+			$this->checked = $checked;
 		}
 
 		public function push(){
-			$db = new db();
+			$values = array();
+			if(isset($this->name)){
+				array_push($values, "Name" => "'" . $this->name . "'");
+			}
+			if(isset($this->checked)){
+				array_push($values, "Checked" => ($checked ? 1, 0));
+			}
+			$db->update(
+				$this->table,
+				$values,
+				$this->id
+			);
 
 		}
 	}
 
 	class list{
 
+		public $name=null;
+		public $id=null;
+		public $color=null;
+		public $archived=null;
+
+		public $items=null;
+
+		private $db=null;
+		private $table=null;
+
+		public function __construct(
+								db $db=null,
+								string $color=null,
+								string $name=null,
+								int $id=null,
+								boolean $archived=null){
+			if(isset($db)){
+				$this->db = $db;
+			}else{
+				$this->db = new db();
+			}
+			$this->table = "lists";
+			$this->color = $color;
+			$this->name = $name;
+			$this->id = $id;
+			$this->archived = $archived;
+			/* generate list of items */
+		}
 	}
 
 	class listsPage extends Page{
